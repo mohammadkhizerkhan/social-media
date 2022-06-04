99104a552842639023c75224bbfcc6d45f528ebf
mohammadkhizerkhan <64767673+mohammadkhizerkhan@users.noreply.github.com>
2022-06-04 11:43:58 +0530

    deleted sidebar file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { SideBar } from "../components";
 import { chakra } from "@chakra-ui/react";
 
 function Home() {
   return (
     <>
-      <SideBar />
       <chakra.h1>This is home</chakra.h1>
     </>
   );
